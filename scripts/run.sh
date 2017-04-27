4b8b333a528a1bc28f4d2238d89bb823b310a861
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2017-04-27 12:01:34 +0300

    run script uses river.genetic

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,7 @@ read binary_id runs cores benchmark_runs rebuild regenerate_corpus <<<$@
 
 RUNS=$runs
 DRIVER_DIR=$(pwd)
+RIVER_GENETIC=$DRIVER_DIR/river.genetic
 TRACER_NODE=$DRIVER_DIR/tracer.node
 NODE_RIVER=$TRACER_NODE/deps/node-river
 
@@ -16,12 +17,14 @@ STATS_FILE=$LOGS_DIR/stats-$cores.csv
 FUZZER_STATS_FILE=$LOGS_DIR/fuzzer-stats-$cores.csv
 LOG_FILE=$LOGS_DIR/run-$cores.log
 FUZZER_LOG_FILE=$LOGS_DIR/fuzzer-$cores.log
+GENETIC_LOG_FILE=$LOGS_DIR/river-genetic-$cores.log
 NO_TESTCASES=0
 
 cleanup() {
   # clean the DRIVER build and stop node
   echo -e "\033[0;32m[DRIVER] Cleaning DRIVER environment ..."; echo -e "\033[0m"
   killall -9 node
+  killall -9 python3
 
   if [ "$rebuild" == "rebuild" ]; then
     cd $TRACER_NODE && rm -rf build && npm install
@@ -42,6 +45,7 @@ cleanup() {
 
   # purge rabbit queues
   sudo rabbitmqctl purge_queue driver.newtests.$binary_id
+  sudo rabbitmqctl purge_queue driver.tracedtests.$binary_id
 }
 
 generate_testcases() {
@@ -53,7 +57,7 @@ generate_testcases() {
       sleep 1
       continue
     fi
-    busy=$(lsof $fuzzer_path)
+    busy=$(lsof $fuzzer_path 2> /dev/null )
     if [ "$busy" == "" ]; then
       break
     fi
@@ -62,13 +66,11 @@ generate_testcases() {
 
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
   rm -f $FUZZER_LOG_FILE
-  $fuzzer_path -close_fd_mask=3 -runs=$RUNS -initial_corpus_db=1 \
-    -dump_all_testcases=1  -dump_to_db=1 -config=$CONFIG_PATH \
-    -binary_id=$binary_id -print_final_stats=1 >> $FUZZER_LOG_FILE 2>&1
+  $fuzzer_path -close_fd_mask=3 -initial_corpus_db=1 \
+     -dump_to_db=1 -config=$CONFIG_PATH \
+    -binary_id=$binary_id -print_final_stats=1 >> $FUZZER_LOG_FILE 2>&1 &
 
-  # $DB_NAME contains $RUNS testcases that will be traced by DRIVER
-  NO_TESTCASES=$(mongo --eval "db.$DB_NAME.count()" | tail -n1)
-  echo -e "\033[0;32m[DRIVER] Generated $NO_TESTCASES testcases in $DB_NAME ..."; echo -e "\033[0m"
+  echo -e "\033[0;32m[DRIVER] Started fuzzer to generate interesting testcases for genetic river ..."; echo -e "\033[0m"
 }
 
 start_tracer() {
@@ -83,14 +85,41 @@ start_tracer() {
   fuzzer_path=$(pwd)/$binary_id/fuzzer
 }
 
+river_genetic_environment() {
+  ## genetic alg setup
+  ## TODO get rid of this stuff
+  export SPARK_HOME=/data/spark/spark-2.0.1-bin-hadoop2.7
+  export PYSPARK_PYTHON=python3
+  export PYSPARK_DRIVER_PYTHON=python3
+  export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.3-src.zip
+  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+  export SPARK_WORKER_DIR=/data/spark_worker_dir
+  export SPARK_EXECUTOR_INSTANCES=2
+  export SCALA_HOME=/usr/share/scala/lib
+  export SPARK_LOCAL_IP=10.18.0.32
+
+  export PYTHONUNBUFFERED=1
+  export SIMPLETRACERLOGSPATH=$RIVER_GENETIC/genetic_code/logs
+  #export LD_LIBRARY_PATH=/data/simpletracer/build-river-tools/lib
+  #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/simpletracer/river.sdk/lin/lib
+  #export SIMPLETRACERPATH=/data/simpletracer/build-river-tools/bin/simple_tracer
+}
+
+start_river_genetic() {
+  echo -e "\033[0;32m[DRIVER] Starting river genetic for $binary_id executable..."; echo -e "\033[0m"
+  cd $RIVER_GENETIC
+  river_genetic_environment
+  ( python3 ./genetic_code/main.py --testsFolderCount 10 --numTasks 1 \
+      --isParallelExecution 0 --populationSize 10 --numberOfIterations 10 \
+      --config $CONFIG_PATH --driver 1  --isDebugEnabled 1 \
+      --executableId $binary_id > $GENETIC_LOG_FILE 2>&1 &)
+}
+
 wait_for_termination() {
   while true; do
     left=$(mongo --eval "db.$DB_NAME.find({state : \"traced\"}).count()" | tail -n1)
     sleep 3 #todo fix this
-    echo "Tracing progress: $left out of $NO_TESTCASES testcases traced"
-    if [ "$left" == "$NO_TESTCASES" ]; then
-      break;
-    fi
+    echo "Tracing progress: found [$left] testcases traced."
   done
 }
 
@@ -133,6 +162,7 @@ main() {
   for i in $(seq $benchmark_runs); do
     cleanup
     start_tracer
+    start_river_genetic
     generate_testcases
     wait_for_termination
     print_statistics
