29b6ca3a52cd8765b9045101e85628161a61f482
Teodor Stoenescu <teodor-stoenescu@users.noreply.github.com>
2017-10-17 16:12:59 +0300

    Fixed catch all routes bug.

--- web.api/server.js
+++ web.api/server.js
@@ -65,7 +65,7 @@ app.use('/api', require('./server/routes/api'));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'web.interface', 'dist'));
 });
 
 app.use(function (err, req, res, next) {
