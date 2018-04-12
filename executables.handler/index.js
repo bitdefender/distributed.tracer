1f5c60f6d98ada777f3e22baa49a9fc5ce2f7009
Mihai-Valentin DUMITRU <mihai.dumitru2201@stud.acs.upb.ro>
2018-04-12 16:44:44 +0300

    Executables handler: add cli support for adding execs

--- executables.handler/index.js
+++ executables.handler/index.js
@@ -4,31 +4,52 @@ if (!common.Init()) {
     process.exit();
 }
 
+const id = process.argv[2];
 const rabbit = common.amqp.GetUrl();
 const amqp = require('amqplib/callback_api');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const mongoURL = common.mongo.GetUrl();
 const dbName = common.mongo.GetDatabase();
 
-function FillQueue(channel, q) {
-  MongoClient.connect(mongoURL, (err, client) => {
-    if (err) {
-      console.log("Error: " + err);
-      return;
-    }
+function EnqueueAll(channel, q, db) {
+	db.collection("executables").find({}, {ObjectId:1}).toArray((err, executables) => {
+		if (err) {
+			console.log("Error: " + err);
+			return;
+		}
 
-    const db = client.db(dbName);
-    db.collection("executables").find({}, {ObjectId:1}).toArray((err, executables) => {
-      if (err) {
-        console.log("Error: " + err);
-        return;
-      }
+		executables.forEach((value) => {
+			channel.sendToQueue(q, new Buffer(value._id.toString()));
+		});
+	});
+}
 
-      executables.forEach((value) => {
-        channel.sendToQueue(q, new Buffer(value._id.toString()));
-      });
-    });
-  });
+function EnqueueOne(channel, q, db, id) {
+	db.collection("executables").findOne({_id: new ObjectID(id)}, (err, document) => {
+		if (err) {
+			console.log("Error: " + err);
+			return;
+		}
+
+		channel.sendToQueue(q, new Buffer(document._id.toString()));
+	});
+}
+
+function FillQueue(channel, q, id) {
+	MongoClient.connect(mongoURL, (err, client) => {
+		if (err) {
+			console.log("Error: " + err);
+			return;
+		}
+
+		const db = client.db(dbName);
+		if (id === undefined) {
+			EnqueueAll(channel, q, db);
+		} else {
+			EnqueueOne(channel, q, db, id);
+		}
+	});
 }
 
 amqp.connect(rabbit, (err, conn) => {
@@ -51,7 +72,14 @@ amqp.connect(rabbit, (err, conn) => {
             }
         });
 
-       FillQueue(channel, q);
+		// this will run at least once, passing "undefined" as the last argument
+		// to "FillQueue", ensuring that it is called to enqueue all executables
+		// when no arguments are provided.
+		let i = 2;
+		do {
+			FillQueue(channel, q, process.argv[i]);
+			++i;
+		} while (i < process.argv.length);
     });
     setTimeout(() => { conn.close(); process.exit(0) }, 500);
 });
